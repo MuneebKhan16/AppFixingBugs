8f6e09a983408e6d2a2dfcdddf5381fe85e62e53
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-04-19 06:13:08 +0500

    changes

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {
   Dimensions,
   Image,
@@ -5,50 +6,43 @@ import {
   Text,
   TouchableOpacity,
   View,
-  ImageBackground
+  ImageBackground,
 } from 'react-native';
-import React, { useState } from 'react';
-import { Colors } from '../config';
-const { width } = Dimensions.get('window');
+import React, {useState} from 'react';
+import {Colors} from '../config';
+const {width} = Dimensions.get('window');
 import Icons from '../assets/Icons';
 import Modal from 'react-native-modal';
 import CustomButton from './CustomButton';
 import Images from '../assets/Images';
-const Categories = (props) => {
-
-
-  console.log('props',props)
+const Categories = props => {
+  console.log('props', props);
   const [isModalVisible, setModalVisible] = useState(false);
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
-  const { category, image, onPress, name, info ,title} = props;
+  const {category, image, onPress, name, info, title} = props;
   return (
     <TouchableOpacity
-    onPress={onPress}
+      onPress={onPress}
       activeOpacity={0.3}
-      style={styles.touchable}
-    >
+      style={styles.touchable}>
       <ImageBackground
         source={Images.background1}
         style={styles.imgbg}
         imageStyle={styles.bg}>
-             <TouchableOpacity
+        <TouchableOpacity
           onPress={toggleModal}
           activeOpacity={0.5}
-          style={styles.icon} >
+          style={styles.icon}>
           <View>
-            <Image
-              source={Icons.privacyPolicy}
-              style={styles.policy}
-            />
+            <Image source={Icons.privacyPolicy} style={styles.policy} />
           </View>
-
-
         </TouchableOpacity>
         <Modal isVisible={isModalVisible}>
           <View style={styles.modal}>
-            <Text style={styles.title}>{name}</Text> </View>
+            <Text style={styles.title}>{name}</Text>{' '}
+          </View>
           <View style={styles.category}>
             <View>
               <Text style={styles.detail}>{info}</Text>
@@ -60,11 +54,8 @@ const Categories = (props) => {
             onPress={toggleModal}
           />
         </Modal>
-     
-        <Text
-          style={styles.ctg}>
-         {title}
-        </Text>
+
+        <Text style={styles.ctg}>{title}</Text>
       </ImageBackground>
     </TouchableOpacity>
   );
@@ -80,21 +71,35 @@ const styles = StyleSheet.create({
     height: 160,
     borderRadius: 20,
     shadowColor: '#000',
-    shadowOffset: { height: 3, width: 3 },
+    shadowOffset: {height: 3, width: 3},
     shadowOpacity: 0.8,
     shadowRadius: 0.5,
-    marginTop: 15
+    marginTop: 15,
   },
-  bg: { borderRadius: 10 },
-  title: { fontSize: 18, color: Colors.white, fontWeight: 'bold' },
-  modal: { backgroundColor: Colors.purple, borderTopLeftRadius: 10, borderTopRightRadius: 10, alignItems: 'center', paddingTop: 10 },
-  category: { backgroundColor: Colors.purple, paddingHorizontal: 8, borderBottomLeftRadius: 10, borderBottomRightRadius: 10, height: 200, alignItems: 'center', justifyContent: 'center' },
-  detail: { color: Colors.white, fontWeight: '700', fontSize: 16 },
+  bg: {borderRadius: 10},
+  title: {fontSize: 18, color: Colors.white, fontWeight: 'bold'},
+  modal: {
+    backgroundColor: Colors.purple,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  category: {
+    backgroundColor: Colors.purple,
+    paddingHorizontal: 8,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  detail: {color: Colors.white, fontWeight: '700', fontSize: 16},
   mdlbtn: {
     alignSelf: 'center',
-    marginTop: 50
+    marginTop: 50,
   },
-  icon:{
+  icon: {
     width: 38,
     height: 38,
     alignItems: 'center',
@@ -103,21 +108,17 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 5,
   },
-  policy:{
+  policy: {
     width: 22,
     height: 22,
     resizeMode: 'contain',
   },
-  ctg:{
+  ctg: {
     color: Colors.white,
     position: 'absolute',
     bottom: 5,
     alignSelf: 'center',
     fontWeight: 'bold',
     fontSize: 20,
-  }
+  },
 });
-
-
-
-
--- src/containers/UserScreens/Home/index.js
+++ src/containers/UserScreens/Home/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import moment from 'moment';
 import React, { Component } from 'react';
 import {
