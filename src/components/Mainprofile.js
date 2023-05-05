e855039943808e6846b14e3e87317fbbd708a374
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-05-06 01:55:41 +0500

    chnages

--- src/components/Mainprofile.js
+++ src/components/Mainprofile.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { Colors, NavService } from '../config';
