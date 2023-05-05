194f42214480497445b060c741bef2f14ab34c5b
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2023-05-05 05:52:19 +0500

    added google autocomplete and firebase

--- src/config/Common/index.js
+++ src/config/Common/index.js
@@ -2,7 +2,8 @@
 const baseURL = 'https://api.myprojectstaging.com/outsideee/api/';
 const socketURL = 'https://api.myprojectstaging.com:3045/';
 const ImageURL = 'https://api.myprojectstaging.com/outsideee/public/';
+const GEOCODE_API_KEY = 'AIzaSyCzeJMBG7dupF95sa6qz5USqXYLJlGpjI4';
 const dummy = 'https://picsum.photos/200/300';
 const defaultTimeout = 30000;
 
-export default {baseURL, socketURL, ImageURL, defaultTimeout , dummy};
+export default {baseURL, socketURL, ImageURL,GEOCODE_API_KEY, defaultTimeout , dummy};
--- src/containers/UserScreens/Home/index.js
+++ src/containers/UserScreens/Home/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable prettier/prettier */
-import moment from 'moment';
 import React, {Component} from 'react';
 import {
   Text,
@@ -20,8 +19,6 @@ import {connect} from 'react-redux';
 import CustomButton from '../../../components/CustomButton';
 import Btn from '../../../components/Btn';
 import Pickdate from '../../../components/Pickdate';
-const {width} = Dimensions.get('window');
-import RNBounceable from '@freakycoder/react-native-bounceable';
 import SplashScreen from 'react-native-splash-screen';
 import Categories from '../../../components/Categories';
 import Images from '../../../assets/Images';
@@ -31,7 +28,7 @@ import {styles} from './Home_Styles';
 import ImageURL from '../../../config/Common';
 import FastImage from 'react-native-fast-image';
 import Searchable from '../../../components/searchable';
-
+import GooglePlaceAutocomplete from '../../../components/GooglePlaceAutocomplete';
 export class Home extends Component {
   Featured = () => {
     NavService.navigate('Featured');
@@ -80,10 +77,10 @@ export class Home extends Component {
     const userImage = this?.props?.user?.image;
 
     const togglePopUp = () => {
-      // if (text != '') {
-      console.log('data', text);
-      this.setState(previousState => ({popUp: !previousState?.popUp}));
-      // }
+      if (text != '') {
+        console.log('data', text);
+        this.setState(previousState => ({popUp: !previousState?.popUp}));
+      }
     };
 
     return (
@@ -152,28 +149,21 @@ export class Home extends Component {
           <View style={styles.modalcontainer}>
             <View>
               <Text style={styles.mdltxt}>Please enter the location</Text>
-
               <Btn />
-              <TouchableOpacity
-                style={styles.mdltxtheader}
-                onPress={() => this.setModalVisible(true)}>
-                <Image
-                  source={Icons.marker}
-                  resizeMode="contain"
-                  style={styles.mdlimg}
-                />
-                <TextInput
-                  editable={false}
-                  style={styles.txtinputadrs}
-                  placeholder="Address"
-                  secureTextEntry={!isFocused}
-                  onFocus={() => this.setState({isFocused: true})}
-                  onBlur={() => this.setState({isFocused: false})}
-                  onChangeText={text => this.setState({text})}
-                  value={text}
-                  placeholderTextColor={Colors.greey}
-                />
-              </TouchableOpacity>
+              <GooglePlaceAutocomplete
+                callback={(address, geometry) =>
+                  console.log('address, geometry', address, geometry)
+                }
+                wrapperStyles={{
+                  width: '100%',
+                }}
+                inputStyles={{
+                  borderWidth: 1,
+                  borderColor: Colors.lightGrey,
+                }}
+                iconColor
+                placeholder={text !== '' ? text : 'Address'}
+              />
               {text.length <= 0 ? (
                 <Text style={{color: Colors?.purple, fontSize: 16}}>
                   Address required
