7a08a21d2ef2e3eb3190263587e2e8f05e985bde
HassanNaeem555 <hassan.naeem@binatedigital.com>
2023-05-18 02:11:11 +0500

    added a carousel on home event screen

--- src/containers/EventScreens/EventHome/index.js
+++ src/containers/EventScreens/EventHome/index.js
@@ -9,93 +9,91 @@ import {
   BackHandler,
   Alert,
 } from 'react-native';
-
-import React, { useState, useEffect, useContext } from 'react';
+import Swiper from 'react-native-swiper';
+import React, {useState, useEffect, useContext} from 'react';
 import AppBackground from '../../../components/AppBackground';
-import { NavService } from '../../../config';
-import { styles } from './eventhome_style';
-import { useSelector } from 'react-redux'
-import ImageURL from '../../../config/Common'
-import dummy from '../../../config/Common'
-import { show_eventCreater_event } from '../../../redux/APIs'
-import Icons from '../../../assets/Icons'
+import {NavService} from '../../../config';
+import {styles} from './eventhome_style';
+import {useSelector} from 'react-redux';
+import ImageURL from '../../../config/Common';
+import dummy from '../../../config/Common';
+import {show_eventCreater_event} from '../../../redux/APIs';
+import Icons from '../../../assets/Icons';
 import eventContext from '../eventContext';
-import FastImage from 'react-native-fast-image'
+import FastImage from 'react-native-fast-image';
 
-const EventHome = (props) => {
-  const { showEvents } = useContext(eventContext);
+const EventHome = props => {
+  const {showEvents} = useContext(eventContext);
 
-  const EventReview = (item) => {
-
-    NavService.navigate('EventReview', item)
+  const EventReview = item => {
+    NavService.navigate('EventReview', item);
   };
 
-
-
-
   return (
-    <AppBackground profile marginHorizontal title={'Home'} home style={{ paddingBottom: 20 }}>
-
-      {
-        showEvents?.length > 0 ?
-          (
-            <View style={styles.container}>
-              <FlatList
-                data={showEvents}
-                showsVerticalScrollIndicator={false}
-                renderItem={({ item, index }) => (
-                  <View style={styles.maincontainer}>
-                    <View style={styles.content}>
-                      <Text
-                        style={styles.title}>
-                        {item?.event_title}
-                      </Text>
-                      <View
-                        style={styles.rev}>
-                        <Image source={Icons.starFilled} style={styles.review} />
-                        <Text
-                          style={styles.txt}>
-                          {item?.rating_avg.map((data) => data.rating_count) || 0}{" "} Reviews
-                        </Text>
-                      </View>
-                    </View>
-                    <TouchableOpacity onPress={() => EventReview(item)}>
-                      <FastImage
-                        source={{ uri: `${ImageURL?.ImageURL}${item?.event_image}` || `${dummy.dummy}` }}
-                        style={styles.imgback}
-                        imageStyle={styles.img}
-                      >
-                        <View style={styles.loc}>
-                          <Image source={Icons.location} resizeMode="contain" style={styles.location} />
-                          <Text style={styles.loctxt} numberOfLines={1} >{' '}{item.event_location}</Text>
-                        </View>
-                      </FastImage>
-                      {/* <ImageBackground
-                         source={{ uri: `${ImageURL?.ImageURL}${item?.event_image}` || `${dummy.dummy}` }}
-                        style={styles.imgback}
-                        imageStyle={styles.img} >
-                        <View style={styles.loc}>
-                          <Image source={Icons.location} resizeMode="contain" style={styles.location} />
-                          <Text style={styles.loctxt}>{' '}{item.event_location}</Text>
-                        </View>
-                      </ImageBackground> */}
-                    </TouchableOpacity>
+    <AppBackground
+      profile
+      marginHorizontal
+      title={'Home'}
+      home
+      style={{paddingBottom: 20}}>
+      {showEvents?.length > 0 ? (
+        <View style={styles.container}>
+          <FlatList
+            data={showEvents}
+            showsVerticalScrollIndicator={false}
+            renderItem={({item, index}) => (
+              <View style={styles.maincontainer}>
+                <View style={styles.content}>
+                  <Text style={styles.title}>{item?.event_title}</Text>
+                  <View style={styles.rev}>
+                    <Image source={Icons.starFilled} style={styles.review} />
+                    <Text style={styles.txt}>
+                      {item?.rating_avg.map(data => data.rating_count) || 0}{' '}
+                      Reviews
+                    </Text>
                   </View>
-                )}
-              />
-            </View>
-
-          ) :
-          (
-            <View style={styles.container1}>
-              <Text style={styles.txtheadersty}>No Events Available</Text>
-            </View>
-          )
-      }
-
+                </View>
+                {item?.images?.length > 0 ? (
+                  <Swiper
+                    style={{height: 180}}
+                    activeDotColor="transparent"
+                    dotColor="transparent">
+                    {item?.images.map((data, index) => (
+                      <TouchableOpacity onPress={() => EventReview(data)}>
+                        <FastImage
+                          key={index}
+                          source={{
+                            uri: `${ImageURL?.ImageURL}${data?.event_images}`,
+                          }}
+                          style={styles.imgback}
+                          imageStyle={styles.img}>
+                          <View style={styles.loc}>
+                            <Image
+                              source={Icons.location}
+                              resizeMode="contain"
+                              style={styles.location}
+                            />
+                            <Text style={styles.loctxt} numberOfLines={1}>
+                              {' '}
+                              {item.event_location}
+                            </Text>
+                          </View>
+                        </FastImage>
+                      </TouchableOpacity>
+                    ))}
+                  </Swiper>
+                ) : null}
+              </View>
+            )}
+          />
+        </View>
+      ) : (
+        <View style={styles.container1}>
+          <Text style={styles.txtheadersty}>No Events Available</Text>
+        </View>
+      )}
     </AppBackground>
   );
 };
 
 export default React.memo(EventHome);
-
