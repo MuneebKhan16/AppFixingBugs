617db5383fb1b3076e78196ca51b570fbb6468ef
HassanNaeem555 <hassan.naeem@binatedigital.com>
2023-05-18 01:53:49 +0500

    added video duration condition to donot upload a video of less than 15 seconds

--- App.js
+++ App.js
@@ -7,19 +7,54 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Provider } from 'react-redux';
-import Toast from 'react-native-toast-message';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Provider} from 'react-redux';
+import Toast, {BaseToast, ErrorToast} from 'react-native-toast-message';
 import Nav from './src';
 import {store} from './src/redux';
-import {Loader} from './src/config';
+import {Loader, Colors} from './src/config';
 LogBox.ignoreAllLogs(true);
 LogBox.ignoreLogs(['Remote debugger']);
+
+const toastConfig = {
+  success: props => (
+    <BaseToast
+      {...props}
+      text1NumberOfLines={5}
+      style={{
+        borderLeftColor: Colors.primary,
+        maxHeight: 120,
+        height: '100%',
+        paddingVertical: 20,
+      }}
+      text1Style={{
+        fontSize: 14,
+        color: Colors.black,
+      }}
+    />
+  ),
+  error: props => (
+    <ErrorToast
+      {...props}
+      text1NumberOfLines={5}
+      style={{
+        borderLeftColor: Colors.google,
+        maxHeight: 120,
+        height: '100%',
+        paddingVertical: 20,
+      }}
+      text1Style={{
+        fontSize: 14,
+        color: Colors.black,
+      }}
+    />
+  ),
+};
 class App extends Component {
   render() {
     return (
       <Wrapper>
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{flex: 1}}>
           <StatusBar
             translucent={true}
             backgroundColor="transparent"
@@ -28,7 +63,7 @@ class App extends Component {
           <Provider store={store}>
             <Loader />
             <Nav />
-            <Toast />
+            <Toast config={toastConfig} />
           </Provider>
         </GestureHandlerRootView>
       </Wrapper>
@@ -38,12 +73,12 @@ class App extends Component {
 
 export default App;
 
-function Wrapper({ children }) {
+function Wrapper({children}) {
   if (Platform.OS === 'ios')
     return (
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+      <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
         {children}
       </KeyboardAvoidingView>
     );
-  return <View style={{ flex: 1 }}>{children}</View>;
+  return <View style={{flex: 1}}>{children}</View>;
 }
